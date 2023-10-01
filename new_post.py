1356f8232db1346a856158185a5956085e804dea
samkirsch10 <samkirsch10@users.noreply.github.com>
2023-09-30 21:33:27 -0400

    swap to inquirer

--- new_post.py
+++ new_post.py
@@ -1,11 +1,27 @@
 #!/usr/bin/env python3
 
-import jinja2
+import itertools
+import inquirer
 from datetime import datetime
+import jinja2
 import os
+import yaml
 
 SCRIPT_DIR_PATH=os.path.dirname(os.path.abspath(__file__))
 
+def gather_categories():
+    categories = list()
+    for _, _, files in os.walk(f"{SCRIPT_DIR_PATH}/docs/recipes/posts"):
+        for recipe_file in files:
+            with open(f"{SCRIPT_DIR_PATH}/docs/recipes/posts/{recipe_file}", 'r') as fh:
+                raw = yaml.safe_load_all(fh)
+                tmp = next(itertools.islice(raw, 0, None))
+                if 'categories' not in tmp:
+                    continue
+                for c in tmp['categories']:
+                    if c not in categories:
+                        categories.append(c)
+    return categories
 
 def render_template(data):
     templateLoader = jinja2.FileSystemLoader(searchpath=SCRIPT_DIR_PATH)
@@ -15,40 +31,31 @@ def render_template(data):
     return template.render(data)
 
 data = dict()
-data["title"] = input("Recipe name: ")
-data["date"] = str(datetime.now())
-data["categories"] = input("Categories (comma-separated): ").split(",")
-site = input("From site (leave empty if not): ")
-if site:
-    data["site"] = site
-data["servings"] = input("Number of servings: ")
-data["total_time"] = input("Cook time (ex. 25 mins): ")
 
-print("Enter/Paste Ingredients. Ctrl-D or Ctrl-Z ( windows ) to save it.")
-data["ingredients"] = []
-while True:
-    try:
-        line = input().lstrip().rstrip()
-    except EOFError:
-        break
-    data["ingredients"].append(line)
+questions = [
+    inquirer.Text('title', message="Recipe name"),
+    inquirer.Checkbox(
+        "categories",
+        message="Categories",
+        choices=gather_categories(),
+    ),
+    inquirer.Text('add_categories', message="New categories (comma-separated)"),
+    inquirer.Text('servings', message="Servings"),
+    inquirer.Text('total_time', message="Cook time (ex. 25 mins)"),
+    inquirer.Editor('ingredients', message="Ingredients"),
+    inquirer.Editor('steps', message="Steps"),
+]
 
-print("Enter/Paste Steps. Ctrl-D or Ctrl-Z ( windows ) to save it.")
-data["steps"] = []
-step_count = 1
-while True:
-    try:
-        line = input().lstrip().rstrip()
-    except EOFError:
-        break
-    if line:
-        data["steps"].append(f"{step_count}. {line}")
-        step_count = step_count + 1
+answers = inquirer.prompt(questions)
+answers['categories'] = answers['categories'] + answers['add_categories'].split(',')
+answers['ingredients'] = answers['ingredients'].split('\n')
+answers['steps'] = [f"{i+1}. {step}" for i, step in enumerate(answers['steps'].split('\n'))]
 
+answers['date'] = str(datetime.now())
 
-file_name = data["title"].replace(" ", "_").replace("'", "_")
+file_name = answers["title"].replace(" ", "_").replace("'", "_")
 loc = f"{SCRIPT_DIR_PATH}/docs/recipes/posts/{file_name}.md"
 with open(loc, "w") as fh:
-    fh.write(render_template(data))
+    fh.write(render_template(answers))
 
 print("Started new recipe file at "+ loc)
